27b9c5258d49300f6b87c4846af0ebd78cd5d3e3
mohaijiang <mohaijiang110@163.com>
2023-07-28 11:01:59 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App a4"/>
+  <HelloWorld msg="Welcome to Your Vue.js App a5"/>
 </template>
 
 <script>
